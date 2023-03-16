d64c72926c8c56734d1b22b741a9a86306ac7fdf
Francisco Ortiz <pacoortiz16@hotmail.com>
2023-03-15 22:08:18 -0600

    feat: starter settings on migrations, seeders and env files

---
+++ database/seeders/20230208032826-users.js
@@ -0,0 +1,58 @@
+'use strict'
+const uuid = require('uuid')
+const { Op } = require('sequelize')
+const { hashPassword } = require('../../libs/bcrypt')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    const usersSeeds = [
+      {
+        id: uuid.v4(),
+        first_name: 'Francisco',
+        last_name: 'Ortiz',
+        email: 'pacoortiz16@hotmail.com',
+        username: 'pacoortiz16@hotmail.com',
+        password: hashPassword('Paco16/07/1994'),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]
+
+    try {
+      await queryInterface.bulkInsert('users', usersSeeds, { transaction })
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    const userNames = [
+      'example@academlo.com',
+    ]
+
+    try {
+      await queryInterface.bulkDelete(
+        'users',
+        {
+          username: {
+            [Op.or]: userNames,
+          },
+        },
+        { transaction }
+      )
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+}
